5572f89c8d84fb06f40f3282d0973c8096519a5f
Mauricio Cavalleri <alu0100683578@ull.edu.es>
2013-11-06 00:14:06 +0000

    Correccion de mas cosas del fichero del spec

--- spec/Matrices_rspec.rb
+++ spec/Matrices_rspec.rb
@@ -1,34 +1,28 @@
 require "Matriz.rb"
 
-describe Matrices do
-
-   before :each do
-      @m = Matrices.new([[1,1,1],[1,1,1],[1,1,1]]) 
-      @ma =Matrices.new([[1,2,4],[6,5,8],[2,5,3]])       
-   end
-
+describe Matriz do
+  before :each do
+      @m = Matriz.new([[1,1,1],[1,1,1],[1,1,1]])        
+  end
 
   it " Se debe poder sumar dos matrices de enteros" do
-    @aux = MatrizEntera.new([[1,1,1],[1,1,1],[1,1,1]])
-    (@m + @aux).matriz.should == [[2,2,2],[2,2,2],[2,2,2]]
+    @aux = Matriz.new([[1,1,1],[1,1,1],[1,1,1]])
+    (@m + @aux).should == [[2,2,2],[2,2,2],[2,2,2]]
   end
-
 
   it " Se debe poder restar dos matrices de enteros" do
-    @aux = MatrizEntera.new([[1,1,1],[1,1,1],[1,1,1]])
-    (@m - @aux).matriz.should == [[0,0,0],[0,0,0],[0,0,0]]
+    @aux = Matriz.new([[1,1,1],[1,1,1],[1,1,1]])
+    (@m - @aux).should == [[0,0,0],[0,0,0],[0,0,0]]
   end
-
-
 
   it " Se debe poder multiplicar dos matrices de enteros" do
-    @aux = MatrizEntera.new([[1,2,3],[1,2,3],[1,2,3]])
-    (@ma * @aux).matriz.should == [[3,6,9],[3,6,9],[3,6,9]]
+    @aux = Matriz.new([[1,2,3],[1,2,3],[1,2,3]])
+    (@m * @aux).should == [[3,6,9],[3,6,9],[3,6,9]]
   end
 
-  it "se debe poder hallar la Traspuesta de una matriz" do
+  it " Se debe poder hacer la traspuesta de una matriz" do
      @aux = Matriz.new([[1,6,2],[2,5,5],[4,8,3]])
-     @m.trasponer.to_s.should eq(@aux.to_s)
+     @aux2 = Matriz.new([[3,8,4],[5,5,2],[2,6,1]])
+     @aux.traspuesta.should == @aux2.matriz
   end
-
 end
